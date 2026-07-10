81cd6d2d96f2d33273982ff2a0f761779cd89d06
ransombiyato <breadman6961@gmail.com>
2026-07-10 22:31:13 +0530

    Improve installer

--- install.sh
+++ install.sh
@@ -4,20 +4,36 @@ set -e
 
 echo "⛏ Installing MCCRAFT..."
 
-if ! command -v curl >/dev/null 2>&1; then
-    pkg install -y curl
+# Check Termux
+if [ -z "$PREFIX" ]; then
+    echo "❌ This is not Termux."
+    exit 1
 fi
 
-TMP=$(mktemp)
+# Install requirements
+echo "📦 Installing requirements..."
+pkg install -y git python
 
-curl -L \
-https://github.com/ransombiyato/mccraft/releases/latest/download/mccraft.deb \
--o "$TMP"
+# Download MCCRAFT
+echo "📥 Downloading MCCRAFT..."
 
-dpkg -i "$TMP"
+rm -rf "$PREFIX/share/mccraft"
 
-rm "$TMP"
+git clone https://github.com/ransombiyato/mccraft.git \
+"$PREFIX/share/mccraft"
+
+# Create command
+echo "🔧 Creating command..."
+
+cat > "$PREFIX/bin/mccraft" << 'EOF'
+#!/data/data/com.termux/files/usr/bin/bash
+python "$PREFIX/share/mccraft/main.py" "$@"
+EOF
+
+chmod +x "$PREFIX/bin/mccraft"
 
 echo
-echo "✅ MCCRAFT installed successfully!"
-echo "Run: mccraft"
+echo "✅ MCCRAFT installed!"
+echo
+echo "Try:"
+echo "  mccraft help"
